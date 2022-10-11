09c6fd829b46941d8aee8cb8640dc63a6dc60e98
Travis Jenkins <Tjinks123@gmail.com>
2022-10-10 19:31:11 -0500

    Most efficient code and a one-liner

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -5,15 +5,7 @@
 """
 
 def main():
-    for i in range(1, 101):
-        # Check for most restrictive first (i.e., multiples of three and five)
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i);
+    # If true it prints the word; otherwise, it prints the number. The "+" will concatenate the words if both are true.
+    for i in range(1, 101): print("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or i)
 
 main()
